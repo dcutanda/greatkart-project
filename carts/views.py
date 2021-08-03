c28cff5570e2d9139c808e217eb50f88903de939
drax <programpractice21@gmail.com>
2021-08-03 11:37:17 +0800

    58. Grouping Cart Item Variations

--- carts/views.py
+++ carts/views.py
@@ -40,25 +40,43 @@ def add_cart(request, product_id):
         cart = Cart.objects.create(cart_id=_cart_id(request))
         cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        if len(product_variation) > 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
-        cart_item.quantity += 1
-        cart_item.save()
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
 
-    except CartItem.DoesNotExist:
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        ex_var_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+
+        # Check if there is existing variation of the item
+        if product_variation in ex_var_list:
+            # increase the cart item quantity
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+        else:
+            # create new cart item with different variation
+            item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            item.save()
+
+    else:
         cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
         if len(product_variation) > 0:
             cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
+            cart_item.variations.add(*product_variation)
         cart_item.save()
 
     # return HttpResponse(cart_item.products)
     # exit()
+    print(product_variation)  # use this to check result
     return redirect('cart')
 
 
@@ -102,6 +120,5 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'tax': tax,
         'grand_total': grand_total
     }
-
 
     return render(request, 'store/cart.html', context)
