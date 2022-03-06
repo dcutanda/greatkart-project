d9d61e840ea13755bfa6ffc5b48632381193992c
drax <programpractice21@gmail.com>
2022-03-06 19:35:45 +0800

    edit settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -149,5 +149,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'ej.cutanda03@gmail.com'
-EMAIL_HOST_PASSWORD = 'VecoPlus'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
