488fa06debf21f8a6e5adf4853750fe17aed1a79
drax <programpractice21@gmail.com>
2023-02-19 11:11:21 +0800

    modified gitignore

--- accounts/views.py
+++ accounts/views.py
@@ -43,17 +43,17 @@ def register(request):
             userprofile.save()
 
             # User Activation
-            current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
-            message = render_to_string('accounts/account_verification_email.html', {
-                'user': user,
-                'domain': current_site,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
-            })
-            to_email = email
-            send_email = EmailMessage(mail_subject, message, to=[to_email])
-            send_email.send()
+            # current_site = get_current_site(request)
+            # mail_subject = 'Please activate your account'
+            # message = render_to_string('accounts/account_verification_email.html', {
+            #     'user': user,
+            #     'domain': current_site,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': default_token_generator.make_token(user),
+            # })
+            # to_email = email
+            # send_email = EmailMessage(mail_subject, message, to=[to_email])
+            # send_email.send()
             # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [abc@gmail.com]')
 
             # this is use to extract request.GET value of command and email to make condition in login template
